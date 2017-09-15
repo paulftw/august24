fa8e8fa2769f75c04a63dd131578b32156209da0
Igor Yeryomin <yeryomin.igor@gmail.com>
2017-09-15 18:30:37 +0300

    Android configuration

--- android/app/src/main/java/com/august24/MainApplication.java
+++ android/app/src/main/java/com/august24/MainApplication.java
@@ -4,16 +4,38 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import io.invertase.firebase.RNFirebasePackage;
+//import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.invertase.firebase.RNFirebasePackage; // <-- Add this line
+// Optional packages - add as appropriate
+//import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
+//import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
+//import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
+//import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
+//import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
+//import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
+//import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
+//import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Performance
+//import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
+
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,8 +47,18 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
-            new RNFirebasePackage()
+          new FBSDKPackage(mCallbackManager),
+          new RNFirebasePackage()
+          // Add these packages as appropriate
+//          new RNFirebaseAdMobPackage(),
+//          new RNFirebaseAnalyticsPackage(),
+//          new RNFirebaseAuthPackage(),
+//          new RNFirebaseRemoteConfigPackage(),
+//          new RNFirebaseCrashPackage(),
+//          new RNFirebaseDatabasePackage(),
+//          new RNFirebaseMessagingPackage(),
+//          new RNFirebasePerformancePackage(),
+//          new RNFirebaseStoragePackage()
       );
     }
   };
@@ -40,5 +72,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+//    FacebookSdk.sdsdkInitialize(getApplicationContext());
   }
 }
