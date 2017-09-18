579e36699676eb3c3499cc06837849a83600dea1
Igor Yeryomin <yeryomin.igor@gmail.com>
2017-09-18 18:22:14 +0300

    Fix android build

--- android/app/src/main/java/com/august24/MainApplication.java
+++ android/app/src/main/java/com/august24/MainApplication.java
@@ -3,7 +3,6 @@ package com.august24;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -43,11 +42,6 @@ public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
     @Override
-    protected String getJSBundleFile() {
-      return CodePush.getJSBundleFile();
-    }
-
-    @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
@@ -56,11 +50,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
-            new LinearGradientPackage(),
-            new ReactNativeContacts(),
-            new FBSDKPackage(),
-            new RNFirebasePackage()
+          new LinearGradientPackage(),
+          new ReactNativeContacts(),
           new FBSDKPackage(mCallbackManager),
           new RNFirebasePackage(),
           // Add these packages as appropriate
